a4cebeeb1bf33d5b555cc4ec708edb0ff2faa74b
dazi <jhd124@hotmail.com>
2017-03-21 18:41:23 +0800

    func: integer the two files into router, it works well

--- router.js
+++ router.js
@@ -1,19 +1,27 @@
 var express= require('express');
 var app=express();
-var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var template = require('art-template');
-var bodyparser = require('body-parser')
+var bodyparser = require('body-parser');
+var movieData = require('./lib/movieDataManipulate');
 
-//设置bodyparser中间件，用以解析post请求
+/*设置bodyparser中间件，用以解析post请求*/
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+
+
+
+
 // ----------------引入模板引擎----------------
 template.config('base', '');
 template.config('extname', '.html');
 app.engine('.html', template.__express);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/public');
+
+
+
+
 // get local IP address
 var os = require('os');
 function getAddress(){
@@ -29,88 +37,19 @@ function getAddress(){
 		return addresses[0];
 	}
 }
-// start a server
 
+
+
+
+// start a server
 var server=app.listen(3000,function(){
 	var addr = getAddress();
 	var port=server.address().port;
 	console.log("listening at %s:%s\r\n",addr,port);
 });
 
-//app.get()
-// connect to mongodb
-var url = 'mongodb://localhost:27017/raccoon';
-var getAllMovieInfo = function(filter,callback){
-	MongoClient.connect(url, function(err,db){
-		assert.equal(null, err);
-		//console.log("Connected successfully to mongodb server\r\n");
 
-	findDocuments(db,filter,function(doc){//注意！ 回调的绝佳示例，先运行这个“findDocuments” 有了doc 再把doc这个参数放进里面的回调函数
-		callback(doc[0]);
-		db.close();
-	});
-});
-}
 
-var getOneMovieInfo = function(filter,callback){
-	MongoClient.connect(url, function(err,db){
-		assert.equal(null, err);
-		// console.log("Connected successfully to mongodb server\r\n");
-
-		findOneDocument(db,filter,function(doc){
-			callback(doc);
-			db.close();
-		})
-	})
-}
-
-//insert a document
-var insertDocuments = function(db,callback){
-	var collection = db.collection('movies');
-
-	collection.insertMany(
-		[{a:1},{a:2}],function(err,result){
-			assert.equal(err,null);
-			assert.equal(2,result.result.n);
-			assert.equal(2,result.ops.length);
-			console.log("Inserted 2 documents into the collection\r\n");
-			callback(result);
-		});
-}
-
-//add a movie into database
-function addOneMovie(data){
-	MongoClient.connect(url, function(err,db){
-		assert.equal(null, err);
-	var collection = db.collection('movies');
-	collection.insertOne(data,function(err){
-		assert.equal(err,null);
-		console.log(new Date().toLocaleString()+"|  Insert the movie \""+data.movieName+"\" \r\n");
-	});
-});
-}
-
-//find all document
-var findDocuments = function(db,fliter,callback){
-	var collection = db.collection('movies');
-	collection.find(fliter).toArray(function(err,docs){
-		assert.equal(err, null);
-		//console.log("found the following records\r\n");
-		console.log(docs);
-		callback(docs);
-	})
-}
-//find a document
-var  findOneDocument = function(db,filter,callback){
-	var collection = db.collection('movies');
-	collection.findOne(filter,function(err,doc){
-		if(err){console.log(err)}
-			else{
-				//console.log("Found the record\r\n");
-		callback(doc);
-	}
-})
-}
 
 // 删除空格
 function deleteSpace(word){
@@ -119,10 +58,11 @@ function deleteSpace(word){
 	return final;
 }
 
-//compiling our schema into a model
-//var Centences=mongoose.model('Centences',englishSchema); 
+
+
 //添加数据库条目
 app.post('/addRecords',function(req,res){
+
 	var data = {
 		movieName: req.body.movieName,
 		movieNameChinese: req.body.movieNameChinese,
@@ -146,17 +86,18 @@ app.post('/addRecords',function(req,res){
 		supportingFemaleRole: req.body.supportingFemaleRole,
 		story: req.body.story
 	}
-	addOneMovie(data);
-	// console.log(res);
+
+	movieData.addOneMovie(data);
+
 	res.end('your data received');
 })
 
 app.get('/movieName/:movieName',function(req,res){
 	var get_filter = {movieName:req.params.movieName};
-	var a=req.params.movieName;
-	getOneMovieInfo({movieName: a},function(doc){
+	var a = req.params.movieName;
+	movieData.findOneMovie({movieName: a},function(doc){
 		
-		var address=getAddress();
+		var address = getAddress();
 		var port = server.address().port;
 		var data = {
 			CSS1: 'http://'+address+':'+port+'/css/bootstrap.min.css',
@@ -195,13 +136,11 @@ app.get('/movieName/:movieName',function(req,res){
 			story: doc.story
 		};
 	  //渲染模板
-	  console.log(new Date().toLocaleString()+'| Client '+req.ip+'| request \"'+doc.movieName+'\" \r\n');
+	 console.log(new Date().toLocaleString()+'| Client '+req.ip+'| request \"'+doc.movieName+'\" \r\n');
 	 res.render('movie',data);
 
 	 res.end();
-		//res.end('director:'+doc.director);
 	});
-	
 })
 
 app.use(express.static('public'));
